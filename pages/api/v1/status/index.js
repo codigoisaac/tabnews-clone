7c5a6027f4ecc6ed86d9a85483a3c8723cbde68c
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-11-28 20:57:35 -0300

    Add max_connections to status API response and update integration test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,11 +6,17 @@ async function status(request, response) {
   const dbVersionQuery = await database.query("SHOW server_version;");
   const dbVersion = dbVersionQuery.rows[0].server_version;
 
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbMaxConnections = parseInt(
+    dbMaxConnectionsQuery.rows[0].max_connections,
+  );
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: dbVersion,
+        max_connections: dbMaxConnections,
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,8 +5,8 @@ test("GET /api/v1/status", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
   expect(response.status).toBe(200);
-  expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
