8b83b030dd7c0a241ed6c3be1c0cec3d7bc5f28e
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-03 18:43:13 -0300

    Refactor migration handling and improve POST method tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,13 +7,25 @@ async function cleanDatabase() {
 }
 
 test("POST /migrations", async () => {
+  // First run - should run migrations
+
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
   const responseBody = await response.json();
 
-  console.log({ responseBody });
-
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(201);
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  // Second run - should not have migrations to run
+
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  const response1Body = await response1.json();
+
+  expect(response1.status).toBe(200);
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBe(0);
 });
