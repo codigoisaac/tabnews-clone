b96cd7bad7f5fb9be80459de025e00666d664516
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-03 19:22:37 -0300

    Refactor database connection handling and update migration logic

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,18 +1,18 @@
-import db from "/infra/database.js";
+import database from "/infra/database.js";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const dbVersionQuery = await db.query("SHOW server_version;");
+  const dbVersionQuery = await database.query("SHOW server_version;");
   const dbVersion = dbVersionQuery.rows[0].server_version;
 
-  const dbMaxConnectionsQuery = await db.query("SHOW max_connections;");
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
   const dbMaxConnections = parseInt(
     dbMaxConnectionsQuery.rows[0].max_connections,
   );
 
   const dbName = process.env.POSTGRES_DB;
-  const dbOpenedConnectionsQuery = await db.query({
+  const dbOpenedConnectionsQuery = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [dbName],
   });
