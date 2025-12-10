42f18853a1730ca6307f31af1f99ef203bc00df1
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-10 13:29:46 -0300

    test: rename all test descriptions to match new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -10,11 +10,15 @@ beforeAll(async () => {
   }
 });
 
-test("GET /migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  const responseBody = await response.json();
+describe("GET to api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Getting pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET /status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET to api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Getting system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-  const responseBody = await response.json();
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  expect(response.status).toBe(200);
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(response.status).toBe(200);
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
