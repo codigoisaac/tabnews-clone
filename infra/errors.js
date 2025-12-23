2ad1dd2145a2038c4affde02333b91faa85dccde
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-22 21:54:04 -0300

    feat: create `NotFoundError` and use in `infra/controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -79,3 +79,24 @@ export class MethodNotAllowedError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "The requested resource was not found.", {
+      cause,
+    });
+
+    this.name = "NotFoundError";
+    this.action = action || "Check if the parameters you sent are correct.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
