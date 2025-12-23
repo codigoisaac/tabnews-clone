ef34a11e08591b7f3544d7df648b18d837738be4
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-22 21:59:25 -0300

    feat: create `user.findOneByUsername` method and `/user/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validadeUniqueEmail(userInputValues.email);
@@ -75,5 +75,36 @@ async function create(userInputValues) {
   }
 }
 
-const userModel = { create };
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "The username provided was not found.",
+        action: "Try a different username.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
+const userModel = { create, findOneByUsername };
 export default userModel;
