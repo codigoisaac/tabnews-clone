e1ff7514cd4366fac4d003f144a4fd99451c769a
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-26 20:08:51 -0300

    feat: get number of hash rounds depending on environment - faster on dev and staging

--- models/password.js
+++ models/password.js
@@ -1,8 +1,16 @@
 import bcryptjs from "bcryptjs";
 
 async function hash(password) {
-  const rounds = 14;
+  const rounds = getNumberOfHashRounds();
   return await bcryptjs.hash(password, rounds);
+}
+
+function getNumberOfHashRounds() {
+  const roundsInEnv = { production: 14, devOrStaging: 1 };
+
+  return process.env.NODE_ENV === "production"
+    ? roundsInEnv.production
+    : roundsInEnv.devOrStaging;
 }
 
 async function compare(providedPassword, storedPassword) {
