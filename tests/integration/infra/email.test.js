93e0085405dd1980760090261af5d3a9d4006937
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-01 19:30:13 -0300

    feat: add `deleteAllEmails()` method to `orchestrator.js` and delete all emails before sending a new

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -1,7 +1,10 @@
+import orchestrator from "tests/orchestrator";
 import email from "infra/email";
 
 describe("infra/email.js", () => {
   test("send()", async () => {
+    await orchestrator.deleteAllEmails();
+
     await email.send({
       from: "Isaac <isaac@isaacmuniz.pro>",
       to: "muniz@isaacmuniz.pro",
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -41,12 +41,20 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function deleteAllEmails() {
+  await fetch(
+    `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}/messages`,
+    { method: "DELETE" },
+  );
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
   createUser,
   createSession,
+  deleteAllEmails,
 };
 
 export default orchestrator;
