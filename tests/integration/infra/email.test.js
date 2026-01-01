22eb94e5b50cfa4681b5433fda3f6c4a514efdec
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-01 19:14:48 -0300

    feat: install `nodemailer` and send test email through it

---
+++ tests/integration/infra/email.test.js
@@ -0,0 +1,12 @@
+import email from "infra/email";
+
+describe("infra/email.js", () => {
+  test("send()", async () => {
+    await email.send({
+      from: "Isaac <isaac@isaacmuniz.pro>",
+      to: "muniz@isaacmuniz.pro",
+      subject: "Test subject",
+      text: "Body test.",
+    });
+  });
+});
