c417d1399e41415a2f716104c038a18803fab7a7
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-06 00:31:34 -0300

    feat: require `create:user` to access `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -131,4 +131,34 @@ describe("POST to api/v1/users", () => {
       });
     });
   });
+
+  describe("Default user", () => {
+    test("With unique and valid data", async () => {
+      const user1 = await orchestrator.createUser();
+      await orchestrator.activateUser(user1);
+      const user1SessionObject = await orchestrator.createSession(user1.id);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${user1SessionObject.token}`,
+        },
+        body: JSON.stringify({
+          username: "loggedUser",
+          email: "loggedUser@isaacmuniz.pro",
+          password: "password123",
+        }),
+      });
+      expect(user2Response.status).toBe(403);
+
+      const user2ResponseBody = await user2Response.json();
+      expect(user2ResponseBody).toEqual({
+        name: "ForbiddenError",
+        message: "You don't have permission to execute this action.",
+        action: "You must gain the correct permissions for this.",
+        status_code: 403,
+      });
+    });
+  });
 });
